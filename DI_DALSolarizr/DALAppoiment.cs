f5707e218a7fffe813d90cbd373c691f2485cda2
jmunoz <rypperx@gmail.com>
2021-11-02 11:53:16 +0100

    Appoiments variados

--- DI_DALSolarizr/DALAppoiment.cs
+++ DI_DALSolarizr/DALAppoiment.cs
@@ -14,16 +14,16 @@ namespace DI_DALSolarizr
         /// <summary>
         /// Method that simulate a connection to a database, returning a list of appoiments (that should be in the database)
         /// </summary>
-        /// <returns> List<Appoiment> getListOfAppoiments() </returns>
+        /// <returns> List<Appoiment> appoiments</returns>
         public static List<Appoiment> getListOfAppoiments()
         {
             List<Appoiment> appoiments = new List<Appoiment>();
-            appoiments.Add(new Appoiment(new DateTime(2000, 12, 3, 9, 30, 00), "Avería", new Costumer("SDK17", "C/Java lang Autoclosable Nº8 ", "74584538")));
-            appoiments.Add(new Appoiment(new DateTime(2000, 12, 13, 9, 30, 00), "Incidencia", new Costumer("Microsoft", "C/Windows Forms Nº2 ", "45471552")));
-            appoiments.Add(new Appoiment(new DateTime(2000, 12, 6, 9, 30, 00), "Avería", new Costumer("TextBox", "C/.setText Nº27", "54784553")));
-            appoiments.Add(new Appoiment(new DateTime(2000, 12, 3, 9, 30, 00), "Avería", new Costumer("SDK17", "C/Java lang Autoclosable Nº8 ", "74584538")));
-            appoiments.Add(new Appoiment(new DateTime(2000, 12, 13, 9, 30, 00), "Incidencia", new Costumer("Microsoft", "C/Windows Forms Nº2 ", "45471552")));
-            appoiments.Add(new Appoiment(new DateTime(2000, 12, 6, 9, 30, 00), "Avería", new Costumer("TextBox", "C/.setText Nº27", "54784553")));
+            appoiments.Add(new Appoiment(new DateTime(2000, 12, 3, 9, 30, 00), "Failure", new Costumer("SDK17", "C/Java lang Autoclosable Nº8 ", "74584538")));
+            appoiments.Add(new Appoiment(new DateTime(2001, 12, 13, 18, 15, 00), "Incidence", new Costumer("Microsoft", "C/Windows Forms Nº2 ", "45471552")));
+            appoiments.Add(new Appoiment(new DateTime(2000, 12, 6, 19, 45, 00), "Failure", new Costumer("TextBox", "C/.setText Nº27", "54784553")));
+            appoiments.Add(new Appoiment(new DateTime(2000, 12, 14, 12, 00, 00), "Instalation", new Costumer("SDK17", "C/Java lang Scanner Nº13 ", "11924538")));
+            appoiments.Add(new Appoiment(new DateTime(2002, 10, 13, 9, 15, 00), "Incidence", new Costumer("Microsoft", "C/ASP NET Core Nº6 ", "65477212")));
+            appoiments.Add(new Appoiment(new DateTime(2000, 12, 6, 11, 30, 00), "Failure", new Costumer("TextBox", "C/.getText Nº42", "819045532")));
             return appoiments;
         }
 
